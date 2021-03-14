8233089a5f6c3acf2ef9f97be40c63facce3ed9d
KerimPotter <48261129+KerimPotter@users.noreply.github.com>
2021-03-14 09:59:00 +0300

    🙀 Added white mod and a lot of things.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const welcomeMessage = document.getElementById("welcomeMessage");
-const welcomeMessages = ["öcüü!", "böö!", "cöö!", "öcü!","bö!", "cö!", ":3"];
-const randomWelcomeMessageSelecter = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
+const welcomeMessage = "bne Kerim ";
+const welcomeMessageId = document.getElementById("welcomeMessage");
+const welcomeMessages = [":3", "öcüü!", "böö!", "cöö!", "öcü!", "bö!", "cö!"];
+let i = 0;
 
-welcomeMessage.innerHTML += randomWelcomeMessageSelecter;
+setTimeout(() => {
+  setInterval(() => {
+    if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6) {
+      welcomeMessageId.innerHTML = welcomeMessage + welcomeMessages[i];
+      if (i == 6) {
+        i = 0;
+      } else i++;
+    };
+  }, 1 * 1000);
+}, 1 * 1000);
